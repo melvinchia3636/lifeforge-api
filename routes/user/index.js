6a762552d97ae294a938814bd790750bafc9c464
= <=>
2024-02-09 08:06:05 +0800

    24w06

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,12 +1,34 @@
 const express = require("express")
 const router = express.Router()
 
+router.patch("/module", async (req, res) => {
+    try {
+        const { pb } = req
+        const { id, data } = req.body
+        const user = await pb.collection("users").update(id, {
+            enabledModules: data
+        })
+        res.json({
+            state: "success",
+            message: "User updated"
+        })
+    } catch (error) {
+        res.status(500).send({
+            state: "error",
+            message: error.message
+        })
+    }
+})
+
 router.patch("/personalization", async (req, res) => {
     try {
         const { pb } = req
         const { id, data } = req.body
         const user = await pb.collection("users").update(id, data)
-        res.json(user)
+        res.json({
+            state: "success",
+            message: "User updated successfully",
+        })
     } catch (error) {
         res.status(500).send({
             state: "error",
