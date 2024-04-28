6a5bcef4f98f3cabb3ad19d872857637111c79bd
= <=>
2024-04-28 16:28:45 +0800

    24w17

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./app"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-_app["default"].listen(process.env.PORT, function () {
-  console.log("Server running on port ".concat(process.env.PORT));
-});
--- src/app.js
+++ src/app.js
@@ -99,6 +99,4 @@ app.use((err, req, res, next) => {
 //     })
 // })
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-});
+export default app
