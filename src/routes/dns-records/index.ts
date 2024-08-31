7a25c7ff8ebffec1a3d9aab75e53aac1f250b193
= <=>
2024-08-31 01:56:09 +0100

    24w35

--- src/routes/dns-records/index.ts
+++ src/routes/dns-records/index.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response } from 'express'
 import asyncWrapper from '../../utils/asyncWrapper.js'
-import { successWithBaseResponse } from '../../utils/response.js'
+import {
+    clientError,
+    serverError,
+    successWithBaseResponse
+} from '../../utils/response.js'
+import { body, param, query } from 'express-validator'
+import hasError from '../../utils/checkError.js'
 
 const router = express.Router()
 
@@ -8,7 +14,7 @@ router.get(
     '/list',
     asyncWrapper(async (req: Request, res: Response) => {
         const url =
-            'https://thecodeblog.net:2083/execute/DNS/parse_zone?zone=thecodeblog.net'
+            'https://cpanel.thecodeblog.net/execute/DNS/parse_zone?zone=thecodeblog.net'
         const headers = {
             Authorization: `cpanel thecodeb:${process.env.CPANEL_API_TOKEN}`
         }
@@ -34,6 +40,34 @@ router.get(
         })
 
         successWithBaseResponse(res, data)
+    })
+)
+
+router.delete(
+    '/',
+    [body('target').isArray(), query('serial').isNumeric()],
+    asyncWrapper(async (req: Request, res: Response) => {
+        if (hasError(req, res)) return
+
+        const { target } = req.body
+        const { serial } = req.query
+
+        const url = `https://cpanel.thecodeblog.net/execute/DNS/mass_edit_zone?zone=thecodeblog.net&serial=${serial}&${target.map((item: string) => `remove=${item}`).join('&')}`
+        const headers = {
+            Authorization: `cpanel thecodeb:${process.env.CPANEL_API_TOKEN}`
+        }
+
+        const response = await fetch(url, { headers })
+        const raw = await response.json()
+
+        console.log(raw)
+
+        if (response.ok && raw.data && !raw.errors) {
+            successWithBaseResponse(res, raw.data.newSerial)
+            return
+        }
+
+        serverError(res, raw.errors[0])
     })
 )
 
--- src/utils/asyncWrapper.ts
+++ src/utils/asyncWrapper.ts
@@ -2,6 +2,11 @@ import { NextFunction, Request, Response } from 'express'
 
 const asyncWrapper =
     (cb: Function) => (req: Request, res: Response, next: NextFunction) =>
-        cb(req, res, next).catch(next)
+        cb(req, res, next).catch(() => {
+            res.status(500).json({
+                state: 'error',
+                message: 'Internal Server Error'
+            })
+        })
 
 export default asyncWrapper
